c239dbbf29223b8434276982f6f88e64dfb15379
Faris Sisic <150537637+FarisSisicSI@users.noreply.github.com>
2024-11-16 11:43:12 +0100

    JobEasy Spring Boot

--- src/main/java/projekat/jobeasy/Controllers/KorisniciController.java
+++ src/main/java/projekat/jobeasy/Controllers/KorisniciController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import projekat.jobeasy.Models.Korisnici;
+import projekat.jobeasy.Models.Pozicije;
 import projekat.jobeasy.Services.KorisnikService;
 
 
@@ -40,6 +41,27 @@ public class KorisniciController {
         korisnikService.izbrisiKorisnika(korisnikId);
         return "redirect:/korisnici";
     }
+
+    @GetMapping("/edit/{korisnikId}")
+    public String editovanjeKorisnika(@PathVariable Long korisnikId, Model model) {
+        korisnikService.pronadjiKorisnikaId(korisnikId).ifPresent(korisnik -> model.addAttribute("korisnik", korisnik));
+        return "korisnik_edit";
+    }
+
+
+    @PostMapping("/edit/{korisnikId}")
+    public String sacuvajKorisnika(@PathVariable Long korisnikId, @ModelAttribute Korisnici korisnik) {
+        korisnikService.pronadjiKorisnikaId(korisnikId).ifPresent(postojeciKorisnik -> {
+            postojeciKorisnik.setIme(korisnik.getIme());
+            postojeciKorisnik.setPrezime(korisnik.getPrezime());
+            postojeciKorisnik.setEmail(korisnik.getEmail());
+            postojeciKorisnik.setUsername(korisnik.getUsername());
+            postojeciKorisnik.setPassword(korisnik.getPassword());
+            korisnikService.sacuvajKorisnika(postojeciKorisnik);
+        });
+        return "redirect:/korisnici";
+    }
+
 
 
     }
