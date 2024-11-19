1ff1fe3adfb55926fce6236b07c77334a7d823c3
Faris Sisic <150537637+FarisSisicSI@users.noreply.github.com>
2024-11-20 00:15:03 +0100

    JobEasy Spring Boot

--- src/main/java/projekat/jobeasy/Controllers/Start.java
+++ src/main/java/projekat/jobeasy/Controllers/Start.java
@@ -8,15 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Start {
 
-    @GetMapping("/") // Handle the root URL
+    @GetMapping("/")
     public String home() {
-        return "welcome"; // Return the home view
+        return "welcome";
     }
 
     @GetMapping("/welcome")
     public String hello(Model model) {
-        return "welcome";  // This maps to the "welcome.html" view
+        return "welcome";
     }
+    @GetMapping("/korisnik")
+    public String korisnikhello(Model model) {
+        return "korisnik_pocetna";
+    }
+    @GetMapping("/admin")
+    public String adminhello(Model model) {
+        return "admin_pocetna";
+    }
+
 
 
 }
