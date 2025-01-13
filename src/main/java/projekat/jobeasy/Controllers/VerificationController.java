47ba9cfe3835a4883eddf8f94e3a4222cd07b111
Faris Sisic <150537637+FarisSisicSI@users.noreply.github.com>
2025-01-13 20:30:29 +0100

    JobEasy Spring Boot

--- src/main/java/projekat/jobeasy/Controllers/VerificationController.java
+++ src/main/java/projekat/jobeasy/Controllers/VerificationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 import projekat.jobeasy.Models.Korisnici;
 import projekat.jobeasy.Models.VerificationToken;
 import projekat.jobeasy.Repositories.KorisniciRepository;
@@ -21,25 +22,30 @@ public class VerificationController {
     private KorisniciRepository userRepository;
 
     @GetMapping("/api/v1/verify")
-    public String verifyAccount(@RequestParam("token") String token) {
+    public RedirectView verifyAccount(@RequestParam("token") String token) {
         // Pronađi token
         VerificationToken verificationToken = tokenRepository.findByToken(token);
 
         if (verificationToken == null) {
-            return "Invalid or expired token. Please check your email for the correct verification link.";
+            RedirectView redirectView = new RedirectView("/login");
+            redirectView.addStaticAttribute("message", "Nevažeći ili istekao token.");
+            return redirectView;
         }
-
 
         // Proveri da li je korisnik već verifikovan
         Korisnici korisnik = verificationToken.getKorisnik();
         if (korisnik.isEnabled()) {
-            return "Your account is already verified. You can log in.";
+            RedirectView redirectView = new RedirectView("/login");
+            redirectView.addStaticAttribute("message", "Vaš račun je već verifikovan! Možete se prijaviti.");
+            return redirectView;
         }
 
         // Verifikuj korisnički nalog
         korisnik.setEnabled(true);
         userRepository.save(korisnik);
 
-        return "Your account has been successfully verified! You can now log in.";
+        RedirectView redirectView = new RedirectView("/login");
+        redirectView.addStaticAttribute("message", "Uspješno ste verifikovali svoj račun! Sada se možete prijaviti!");
+        return redirectView;
     }
 }
