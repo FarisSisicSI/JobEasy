78cd87d75ddc33e962e9cb6acd0781d2e1de03bc
Faris Sisic <150537637+FarisSisicSI@users.noreply.github.com>
2025-01-31 02:51:56 +0100

    JobEasy Spring Boot

--- src/main/java/projekat/jobeasy/Repositories/PrijavaRepository.java
+++ src/main/java/projekat/jobeasy/Repositories/PrijavaRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import projekat.jobeasy.Models.Prijava;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface PrijavaRepository extends JpaRepository<Prijava, Long> {
