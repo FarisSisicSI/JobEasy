60ceb63dd1cbe97bc8fe29aed5f19c23a8462ce6
Faris Sisic <150537637+FarisSisicSI@users.noreply.github.com>
2025-02-03 16:54:57 +0100

    JobEasy Spring Boot

--- src/main/java/projekat/jobeasy/Models/Korisnici.java
+++ src/main/java/projekat/jobeasy/Models/Korisnici.java
@@ -7,8 +7,6 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-
-
 @Entity
 @Getter
 @Setter
@@ -26,6 +24,17 @@ public class Korisnici {
     private String prezime;
 
     @NotNull
+    private String adresa;
+
+    @NotNull
+    @Size(min = 9, max = 15)
+    private String telefon;
+
+    private String vozackaDozvola;
+
+    private String cv;
+
+    @NotNull
     @Email
     private String email;
 
@@ -37,25 +46,44 @@ public class Korisnici {
     @Size(min = 3)
     private String password;
 
+    @ManyToOne
+    @JoinColumn(name = "id_mjesto", nullable = false)
+    private Opcina opcina;
+
+    @ManyToOne
+    @JoinColumn(name = "id_zanimanja1", nullable = false)
+    private Zanimanje zanimanje1;
+
+    @ManyToOne
+    @JoinColumn(name = "id_zanimanja2")
+    private Zanimanje zanimanje2;
+
     private Integer idRole = 1;
+
     @Column(nullable = false, columnDefinition = "boolean default false")
     private boolean enabled = false;
 
     @OneToOne(mappedBy = "korisnik", cascade = CascadeType.ALL, orphanRemoval = true)
     private VerificationToken verificationToken;
 
-
-
     public Korisnici() {}
 
-    public Korisnici(Long id, String ime, String prezime, String email, String username, String password, Integer idRole) {
+    public Korisnici(Long id, String ime, String prezime, String adresa, String telefon, String vozackaDozvola,
+                     String cv, String email, String username, String password, Opcina opcina, Zanimanje zanimanje1,
+                     Zanimanje zanimanje2, Integer idRole) {
         this.id = id;
         this.ime = ime;
         this.prezime = prezime;
+        this.adresa = adresa;
+        this.telefon = telefon;
+        this.vozackaDozvola = vozackaDozvola;
+        this.cv = cv;
         this.email = email;
         this.username = username;
         this.password = password;
+        this.opcina = opcina;
+        this.zanimanje1 = zanimanje1;
+        this.zanimanje2 = zanimanje2;
         this.idRole = idRole != null ? idRole : 1;
-
     }
 }
